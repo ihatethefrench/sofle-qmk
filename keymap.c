1ad9913c6d17feee37b4fc562820f3365ea6a43e
jan Michal <michal@tar.black>
2022-05-05 19:18:42 +0100

    gesc

--- keymap.c
+++ keymap.c
@@ -22,22 +22,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * WORKMAN
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
+ * | `ESC |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | Prnt |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | ESC  |   Q  |   D  |   R  |   W  |   B  |                    |   J  |   F  |   U  |   P  |   ;  | Bspc |
+ * | Tab  |   Q  |   D  |   R  |   W  |   B  |                    |   J  |   F  |   U  |   P  |   ;  | Bspc |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | TAB  |   A  |   S  |   H  |   T  |   G  |-------.    ,-------|   Y  |   N  |   E  |   O  |   I  |  '   |
+ * | Caps |   A  |   S  |   H  |   T  |   G  |-------.    ,-------|   Y  |   N  |   E  |   O  |   I  |  '   |
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   M  |   C  |   V  |-------|    |-------|   K  |   L  |   ,  |   .  |   /  |RShift|
- * `-----------------------------------------/       /     \      \-----------------------------------------'
+i * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            | LGUI | LAlt | LCTR |LOWER | /Enter  /       \Space \  |RAISE | RCTR | RAlt | RGUI |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
 [_WORKMAN] = LAYOUT(
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,     KC_5,                               KC_6,     KC_7,     KC_8,    KC_9,   KC_0,    KC_GRV,
-  KC_ESC,   KC_Q,   KC_D,    KC_R,    KC_W,     KC_B,                               KC_J,     KC_F,     KC_U,    KC_P,   KC_SCLN, KC_BSPC,
-  KC_TAB,   KC_A,   KC_S,    KC_H,    KC_T,     KC_G,                               KC_Y,     KC_N,     KC_E,    KC_O,   KC_I,    KC_QUOT,
+  KC_GESC,  KC_1,   KC_2,    KC_3,    KC_4,     KC_5,                               KC_6,     KC_7,     KC_8,    KC_9,   KC_0,    KC_PSCR,
+  KC_TAB,   KC_Q,   KC_D,    KC_R,    KC_W,     KC_B,                               KC_J,     KC_F,     KC_U,    KC_P,   KC_SCLN, KC_BSPC,
+  KC_CAPS,  KC_A,   KC_S,    KC_H,    KC_T,     KC_G,                               KC_Y,     KC_N,     KC_E,    KC_O,   KC_I,    KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_M,    KC_C,     KC_V,     KC_MUTE,     LALT(KC_F2), KC_K,     KC_L,     KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
                     KC_LGUI, KC_LALT, KC_LCTRL, KC_LOWER, KC_ENT,      KC_SPC,      KC_RAISE, KC_RCTRL, KC_RALT, KC_RGUI
 ),
@@ -45,11 +45,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
+ * | `ESC |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | Prnt |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | ESC  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | Bspc |
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | Bspc |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * | Caps |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -58,9 +58,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [_QWERTY] = LAYOUT(
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,     KC_5,                               KC_6,     KC_7,     KC_8,    KC_9,   KC_0,    KC_GRV,
-  KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,     KC_T,                               KC_Y,     KC_U,     KC_I,    KC_O,   KC_P,    KC_BSPC,
-  KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,     KC_G,                               KC_H,     KC_J,     KC_K,    KC_L,   KC_SCLN, KC_QUOT,
+  KC_GESC,  KC_1,   KC_2,    KC_3,    KC_4,     KC_5,                               KC_6,     KC_7,     KC_8,    KC_9,   KC_0,    KC_PSCR,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,     KC_T,                               KC_Y,     KC_U,     KC_I,    KC_O,   KC_P,    KC_BSPC,
+  KC_CAPS,  KC_A,   KC_S,    KC_D,    KC_F,     KC_G,                               KC_H,     KC_J,     KC_K,    KC_L,   KC_SCLN, KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,     KC_B,     KC_MUTE,     LALT(KC_F2), KC_N,     KC_M,     KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
                     KC_LGUI, KC_LALT, KC_LCTRL, KC_LOWER, KC_SPC,      KC_ENT,      KC_RAISE, KC_RCTRL, KC_RALT, KC_RGUI
 ),
@@ -249,5 +249,3 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     }
     return true;
 }
-
-#endif
