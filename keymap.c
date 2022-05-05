5b1d52d3113bd402833850aa5aef3a71f1e7aa16
jan Michal <michal@tar.black>
2022-05-05 20:36:00 +0100

    gesc fix

--- keymap.c
+++ keymap.c
@@ -35,7 +35,7 @@ i * `-----------------------------------------/       /     \      \------------
  *            `----------------------------------'           '------''---------------------------'
  */
 [_WORKMAN] = LAYOUT(
-  KC_GESC,  KC_1,   KC_2,    KC_3,    KC_4,     KC_5,                               KC_6,     KC_7,     KC_8,    KC_9,   KC_0,    KC_PSCR,
+  QK_GESC,  KC_1,   KC_2,    KC_3,    KC_4,     KC_5,                               KC_6,     KC_7,     KC_8,    KC_9,   KC_0,    KC_PSCR,
   KC_TAB,   KC_Q,   KC_D,    KC_R,    KC_W,     KC_B,                               KC_J,     KC_F,     KC_U,    KC_P,   KC_SCLN, KC_BSPC,
   KC_CAPS,  KC_A,   KC_S,    KC_H,    KC_T,     KC_G,                               KC_Y,     KC_N,     KC_E,    KC_O,   KC_I,    KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_M,    KC_C,     KC_V,     KC_MUTE,     LALT(KC_F2), KC_K,     KC_L,     KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
@@ -58,7 +58,7 @@ i * `-----------------------------------------/       /     \      \------------
  *            `----------------------------------'           '------''---------------------------'
  */
 [_QWERTY] = LAYOUT(
-  KC_GESC,  KC_1,   KC_2,    KC_3,    KC_4,     KC_5,                               KC_6,     KC_7,     KC_8,    KC_9,   KC_0,    KC_PSCR,
+  QK_GESC,  KC_1,   KC_2,    KC_3,    KC_4,     KC_5,                               KC_6,     KC_7,     KC_8,    KC_9,   KC_0,    KC_PSCR,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,     KC_T,                               KC_Y,     KC_U,     KC_I,    KC_O,   KC_P,    KC_BSPC,
   KC_CAPS,  KC_A,   KC_S,    KC_D,    KC_F,     KC_G,                               KC_H,     KC_J,     KC_K,    KC_L,   KC_SCLN, KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,     KC_B,     KC_MUTE,     LALT(KC_F2), KC_N,     KC_M,     KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
